8b41d272ec020fd4c8530dbb5e1026551e428e42
Ahmet-J <3354080axmed04@gmail.com>
2026-05-19 00:39:18 +0300

    fix: root directory config

--- sourcing web app/src/config/env.ts
+++ sourcing web app/src/config/env.ts
@@ -7,6 +7,7 @@ export const config = {
   JWT_SECRET: process.env.JWT_SECRET || 'fallback_secret_change_me',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '100d',
   CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:3000',
+  FRONTEND_URL: process.env.FRONTEND_URL || 'https://b2b-jk2e.vercel.app',
   
   EMAIL_HOST: process.env.EMAIL_HOST || 'smtp.gmail.com',
   EMAIL_PORT: Number(process.env.EMAIL_PORT) || 587,
